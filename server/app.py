9b7e425571685f62cebfddc3cea3f9984c7883c0
Aleksandr <titamoto@gmail.com>
2023-09-10 14:10:59 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -46,7 +46,38 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        for key in data:
+            setattr(plant, key, data[key])
+        
+        db.session.add(plant)
+        db.session.commit()
 
+        response = make_response(
+            plant.to_dict(),
+            200
+        )
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": ""    
+        }
+
+        response = make_response(
+            response_body,
+            204
+        )
+
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
